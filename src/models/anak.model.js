647c1279df597142519590ab2b6f8f8acb1fb8fa
ucuo <fajarmufid01@gmail.com>
2025-08-31 10:53:50 +0700

    fixing anak

--- src/models/anak.model.js
+++ src/models/anak.model.js
@@ -1,12 +1,40 @@
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+
 const Anak = sequelize.define('Anak', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    nama: { type: DataTypes.STRING, allowNull: false },
-    tanggal_lahir: { type: DataTypes.DATEONLY, allowNull: false },
-    berat_badan: { type: DataTypes.DECIMAL(5, 2) },
-    tinggi_badan: { type: DataTypes.DECIMAL(5, 2) },
-}, { tableName: 'anak', timestamps: false });
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    nama: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    gender: {
+        type: DataTypes.ENUM('male', 'female'),
+        allowNull: false,
+    },
+    tanggalLahir: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        field: 'tanggal_lahir'
+    },
+    tinggiBadan: {
+        type: DataTypes.DECIMAL(5, 2),
+        field: 'tinggi_badan'
+    },
+    beratBadan: {
+        type: DataTypes.DECIMAL(5, 2),
+        field: 'berat_badan'
+    }
+}, {
+    tableName: 'anak',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: false,
+});
 
 module.exports = Anak;
